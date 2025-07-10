ea9fdefa6c86b5652aa6eea8242614308e39a5b5
Taewon Song (Home) <crazytb@gmail.com>
2025-07-11 04:26:26 +0900

    Ch1 -> Ch0으로 NPCA 전송시 점유하는 프레임 슬롯 길이에 문제 있음. (obss_simulation_npca_short_frame_duration.csv 참고)

--- random_access/configs.py
+++ random_access/configs.py
@@ -1,11 +1,32 @@
 # Configuration
 
-simulation_time = 10000  # Total simulation time in slots
+simulation_time = 10000  # Total simulation time in us
+simulation_slot = simulation_time // 9  # Slot duration in us (9us for 802.11ax)
 frame_size = 33  # Size of each frame in slots
 
 simulation_configs = [
     {
-        "label": "Default Frame Duration", 
+        "label": "No NPCA Short Frame Duration", 
+        "num_channels": 2,
+        "stas_per_channel": [10, 10],
+        "simulation_time": simulation_time,
+        "frame_size": frame_size,
+        "obss_enabled_per_channel": [False, True],  # 채널 1에서만 OBSS 활성화
+        "npca_enabled": [False, False],  # 채널 1에서만 NPCA 활성화
+        "obss_generation_rate": 0.01
+    },
+    {
+        "label": "No NPCA Long Frame Duration",
+        "num_channels": 2,
+        "stas_per_channel": [10, 10],
+        "simulation_time": simulation_time,
+        "frame_size": frame_size*10,
+        "obss_enabled_per_channel": [False, True],  # 채널 1에서만 OBSS 활성화
+        "npca_enabled": [False, False],  # 채널 1에서만 NPCA 활성화
+        "obss_generation_rate": 0.01
+    },
+    {
+        "label": "NPCA Short Frame Duration", 
         "num_channels": 2,
         "stas_per_channel": [10, 10],
         "simulation_time": simulation_time,
@@ -15,11 +36,11 @@ simulation_configs = [
         "obss_generation_rate": 0.01
     },
     {
-        "label": "Long Frame Duration",
+        "label": "NPCA Long Frame Duration",
         "num_channels": 2,
         "stas_per_channel": [10, 10],
         "simulation_time": simulation_time,
-        "frame_size": frame_size*100,
+        "frame_size": frame_size*10,
         "obss_enabled_per_channel": [False, True],  # 채널 1에서만 OBSS 활성화
         "npca_enabled": [False, True],  # 채널 1에서만 NPCA 활성화
         "obss_generation_rate": 0.01
