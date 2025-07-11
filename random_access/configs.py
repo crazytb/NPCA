b787a277df0bd16d8c3c6a60690900f58f8cba4f
Taewon Song (Home) <crazytb@gmail.com>
2025-07-11 10:42:26 +0900

    Comment out unused simulation configurations for clarity and future reference

--- random_access/configs.py
+++ random_access/configs.py
@@ -5,26 +5,26 @@ simulation_slot = simulation_time // 9  # Slot duration in us (9us for 802.11ax)
 frame_size = 33  # Size of each frame in slots
 
 simulation_configs = [
-    {
-        "label": "No NPCA Short Frame Duration", 
-        "num_channels": 2,
-        "stas_per_channel": [10, 10],
-        "simulation_time": simulation_time,
-        "frame_size": frame_size,
-        "obss_enabled_per_channel": [False, True],  # 채널 1에서만 OBSS 활성화
-        "npca_enabled": [False, False],  # 채널 1에서만 NPCA 활성화
-        "obss_generation_rate": 0.01
-    },
-    {
-        "label": "No NPCA Long Frame Duration",
-        "num_channels": 2,
-        "stas_per_channel": [10, 10],
-        "simulation_time": simulation_time,
-        "frame_size": frame_size*10,
-        "obss_enabled_per_channel": [False, True],  # 채널 1에서만 OBSS 활성화
-        "npca_enabled": [False, False],  # 채널 1에서만 NPCA 활성화
-        "obss_generation_rate": 0.01
-    },
+    # {
+    #     "label": "No NPCA Short Frame Duration", 
+    #     "num_channels": 2,
+    #     "stas_per_channel": [10, 10],
+    #     "simulation_time": simulation_time,
+    #     "frame_size": frame_size,
+    #     "obss_enabled_per_channel": [False, True],  # 채널 1에서만 OBSS 활성화
+    #     "npca_enabled": [False, False],  # 채널 1에서만 NPCA 활성화
+    #     "obss_generation_rate": 0.01
+    # },
+    # {
+    #     "label": "No NPCA Long Frame Duration",
+    #     "num_channels": 2,
+    #     "stas_per_channel": [10, 10],
+    #     "simulation_time": simulation_time,
+    #     "frame_size": frame_size*10,
+    #     "obss_enabled_per_channel": [False, True],  # 채널 1에서만 OBSS 활성화
+    #     "npca_enabled": [False, False],  # 채널 1에서만 NPCA 활성화
+    #     "obss_generation_rate": 0.01
+    # },
     {
         "label": "NPCA Short Frame Duration", 
         "num_channels": 2,
@@ -35,16 +35,16 @@ simulation_configs = [
         "npca_enabled": [False, True],  # 채널 1에서만 NPCA 활성화
         "obss_generation_rate": 0.01
     },
-    {
-        "label": "NPCA Long Frame Duration",
-        "num_channels": 2,
-        "stas_per_channel": [10, 10],
-        "simulation_time": simulation_time,
-        "frame_size": frame_size*10,
-        "obss_enabled_per_channel": [False, True],  # 채널 1에서만 OBSS 활성화
-        "npca_enabled": [False, True],  # 채널 1에서만 NPCA 활성화
-        "obss_generation_rate": 0.01
-    }
+    # {
+    #     "label": "NPCA Long Frame Duration",
+    #     "num_channels": 2,
+    #     "stas_per_channel": [10, 10],
+    #     "simulation_time": simulation_time,
+    #     "frame_size": frame_size*10,
+    #     "obss_enabled_per_channel": [False, True],  # 채널 1에서만 OBSS 활성화
+    #     "npca_enabled": [False, True],  # 채널 1에서만 NPCA 활성화
+    #     "obss_generation_rate": 0.01
+    # }
 ]
 
 # simulation_configs = [
